c6e49d1fff5645cc3ddc0421f018a371af565495
oier <oierbravo@gmail.com>
2022-11-28 16:36:25 +0100

    Allow tags

--- src/main/java/com/oierbravo/createsifter/foundation/data/recipe/SiftingRecipeGen.java
+++ src/main/java/com/oierbravo/createsifter/foundation/data/recipe/SiftingRecipeGen.java
@@ -7,12 +7,13 @@ import com.simibubi.create.AllItems;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.Tags;
 
 public class SiftingRecipeGen extends ModProcessingRecipes {
     GeneratedRecipe GRAVEL_ANDESITE = create("gravel_andesite_mesh", b -> b.duration(500)
 
             .require(MeshTypes.ANDESITE.getItem())
-            .require(Blocks.GRAVEL)
+            .require(Tags.Items.GRAVEL)
             .output(.1f, AllItems.COPPER_NUGGET::get,1)
             .output(.1f, AllItems.ZINC_NUGGET::get,1)
             .output(.05f, Items.IRON_NUGGET,1)
@@ -23,7 +24,7 @@ public class SiftingRecipeGen extends ModProcessingRecipes {
     GeneratedRecipe GRAVEL_ZINC = create("gravel_zinc_mesh", b -> b.duration(500)
 
             .require(MeshTypes.ZINC.getItem())
-            .require(Blocks.GRAVEL)
+            .require(Tags.Items.GRAVEL)
             .output(.15f, AllItems.COPPER_NUGGET::get,1)
             .output(.15f, AllItems.ZINC_NUGGET::get,1)
             .output(.1f, Items.IRON_NUGGET,1)
@@ -36,7 +37,7 @@ public class SiftingRecipeGen extends ModProcessingRecipes {
     GeneratedRecipe GRAVEL_BRASS = create("gravel_brass_mesh", b -> b.duration(500)
 
             .require(MeshTypes.BRASS.getItem())
-            .require(Blocks.GRAVEL)
+            .require(Tags.Items.GRAVEL)
             .output(.20f, AllItems.CRUSHED_COPPER::get,1)
             .output(.20f, AllItems.CRUSHED_ZINC::get,1)
             .output(.15f, AllItems.CRUSHED_GOLD::get,1)
@@ -47,27 +48,27 @@ public class SiftingRecipeGen extends ModProcessingRecipes {
             .output(.15f, AllItems.EXP_NUGGET.get(), 1));
 
     GeneratedRecipe SAND_STRING = create("sand_string_mesh", b -> b.duration(500)
-            .require(Blocks.SAND)
+            .require(Tags.Items.SAND)
             .require(MeshTypes.STRING.getItem())
             .output(.05f, Items.REDSTONE,1)
             .output(.4f, Items.BONE_MEAL,1));
 
     GeneratedRecipe SAND_ANDESITE = create("sand_andesite_mesh", b -> b.duration(500)
-            .require(Blocks.SAND)
+            .require(Tags.Items.SAND)
             .require(MeshTypes.ANDESITE.getItem())
             .output(.1f, Items.REDSTONE,1)
             .output(.4f, Items.BONE_MEAL,1)
             .output(.10f, AllItems.EXP_NUGGET.get(), 1));
 
     GeneratedRecipe SAND_ZINC = create("sand_zinc_mesh", b -> b.duration(500)
-            .require(Blocks.SAND)
+            .require(Tags.Items.SAND)
             .require(MeshTypes.ZINC.getItem())
             .output(.15f, Items.REDSTONE,1)
             .output(.4f, Items.BONE_MEAL,1)
             .output(.10f, AllItems.EXP_NUGGET.get(), 1));
 
     GeneratedRecipe SAND_BRASS = create("sand_brass_mesh", b -> b.duration(500)
-            .require(Blocks.SAND)
+            .require(Tags.Items.SAND)
             .require(MeshTypes.BRASS.getItem())
             .output(.25f, Items.REDSTONE,2)
             .output(.10f, Items.GLOWSTONE_DUST,1)
