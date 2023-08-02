dcf8976ef0078ce440f1fbb6527bbb8a693aa9bf
oier <oierbravo@gmail.com>
2023-08-02 03:49:21 +0200

    It's ponder time

--- src/main/java/com/oierbravo/createsifter/CreateSifter.java
+++ src/main/java/com/oierbravo/createsifter/CreateSifter.java
@@ -2,16 +2,17 @@ package com.oierbravo.createsifter;
 
 import com.oierbravo.createsifter.foundation.data.recipe.ModProcessingRecipeGen;
 import com.oierbravo.createsifter.groups.ModGroup;
-import com.oierbravo.createsifter.register.ModBlocks;
-import com.oierbravo.createsifter.register.ModConfigs;
-import com.oierbravo.createsifter.register.ModItems;
-import com.oierbravo.createsifter.register.ModBlockEntities;
+import com.oierbravo.createsifter.register.*;
+import com.simibubi.create.CreateClient;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -53,6 +54,13 @@ public class CreateSifter {
         registrate().addRawLang("create.recipe.sifting", "Sifting recipe");
         registrate().addRawLang("create.createsifter.recipe.sifting.minimumspeed", "%1$s RPM");
         registrate().addRawLang("itemGroup.createsifter:main", "Create sifting");
+        //Ponder
+        registrate().addRawLang("createsifter.ponder.sifter.header", "Block sifting");
+        registrate().addRawLang("createsifter.ponder.sifter.text_1", "Sifter process items by sifting them");
+        registrate().addRawLang("createsifter.ponder.sifter.text_2", "They can be powered from the side using cogwheels");
+        registrate().addRawLang("createsifter.ponder.sifter.text_3", "Throw or Insert items at the top");
+        registrate().addRawLang("createsifter.ponder.sifter.text_4", "After some time, the result can be obtained via Right-click");
+        registrate().addRawLang("createsifter.ponder.sifter.text_5", "The outputs can also be extracted by automation");
     }
     public static CreateRegistrate registrate() {
         return REGISTRATE;
@@ -69,7 +77,7 @@ public class CreateSifter {
 
     }
     private void doClientStuff(final FMLClientSetupEvent event) {
-       // event.enqueueWork(ModPonders::register);
+       event.enqueueWork(ModPonders::register);
     }
 
     public static ResourceLocation asResource(String path) {
